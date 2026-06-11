8c11c82875137e28dcb4508ad888a80d3b46dabd
MahidharReddy553 <mahidharreddy158@gmail.com>
2026-06-11 21:30:00 +0530

    corrected logic

--- week_1/day_2/check_sorted.py
+++ week_1/day_2/check_sorted.py
@@ -1,5 +1,5 @@
 li = [7, 45, 69, 1, 46, 63, 3]
-# li.sort()
+li.sort()
 is_sorted = True
 
 e = li[0]
@@ -7,5 +7,7 @@ for i in range(1, len(li)-1):
     if e > li[i]:
         is_sorted = False
         break
-
+    elif e <= li[i]:
+        e = li[i]
+        
 print(is_sorted)
