3ec2af7f53f7507ffdf4c9e381ec98eea09a6989
MahidharReddy553 <mahidharreddy158@gmail.com>
2026-06-28 16:56:52 +0530

    implmented First‑element pivot partition

--- week_2/day_9/quick_sort.py
+++ week_2/day_9/quick_sort.py
@@ -5,10 +5,10 @@ def quick_sort(arr, l = 0, h = None):
         h = len(arr) - 1
 
     if l < h:
-        p = partition(arr, l, h)
+        p = f_partition(arr, l, h)
         quick_sort(arr, l, p-1)
         quick_sort(arr, p+1, h)
-        return arr
+    return arr
 
 def partition(arr, l, h):
     p = arr[h]
@@ -19,4 +19,24 @@ def partition(arr, l, h):
             arr[i], arr[j] = arr[j], arr[i]
     arr[i+1], arr[h] = arr[h], arr[i + 1]
     return i + 1
+
+
+def f_partition(arr, l, h):
+    p = arr[l]
+    i = l 
+    j = h
+
+    while True:
+        while arr[i] <= p and i < h:
+            i += 1
+        while arr[j] >=p and j > l :
+            j -= 1
+        if i < j:
+            arr[i], arr[j] = arr[j], arr[i]
+        else:
+            break
+        
+    arr[l], arr[j] = arr[j], arr[l]
+    return j
+
 print(quick_sort(arr))
